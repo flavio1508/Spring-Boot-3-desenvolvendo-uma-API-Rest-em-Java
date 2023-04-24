127194a92cff104df6a6b745339f7a3f2a9a8fc7
flavio1508 <flavioaugusto1508@gmail.com>
2023-04-24 08:54:47 -0300

    first commit

---
+++ api/src/main/java/med/voll/api/controller/HelloController.java
@@ -0,0 +1,18 @@
+package med.voll.api.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
+@RestController
+@RequestMapping("/hello")
+public class HelloController {
+    
+    @GetMapping
+    public String olaMundo() {
+        return "Hello World!";
+    }
+    
+}
