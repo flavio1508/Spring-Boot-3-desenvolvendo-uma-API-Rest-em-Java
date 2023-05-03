ea2dd62ffca548b760bcd5ad2c06e6e17dbe85f0
flavio1508 <flavioaugusto1508@gmail.com>
2023-05-03 10:54:57 -0300

    att

--- api/src/main/java/med/voll/api/controller/MedicoController.java
+++ api/src/main/java/med/voll/api/controller/MedicoController.java
@@ -1,15 +1,65 @@
 package med.voll.api.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.data.web.SpringDataWebProperties.Pageable;
+import org.springframework.data.domain.Page;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import jakarta.validation.Valid;
+import med.voll.api.medico.DadosAtualizacaoMedico;
+import med.voll.api.medico.DadosCadastroMedico;
+import med.voll.api.medico.Medico;
+import med.voll.api.medico.MedicoRepository;
+
 @RestController
 @RequestMapping("medicos")
 public class MedicoController {
-    @PostMapping
-    public void cadastrar(@RequestBody String json){//metodo
-      System.out.println(json);
-    }
+
+  @Autowired
+  private MedicoRepository repository;
+
+  @PostMapping
+  @Transactional
+  public void cadastrar(@RequestBody DadosCadastroMedico dados) {// metodo
+    repository.save(new Medico(dados));
+  }
+
+  // @GetMapping
+  // public List<DadosListagemMedico> listar() {
+  //   return repository.findAll().stream().map(DadosListagemMedico :: new).toList();//convertendo de medico para DadosListagemMedico lista de medico
+  // }
+  @GetMapping
+    public Page<DadosListagemMedico> listar(@PageableDefault(size = 10, sort = {"nome"})Pageable paginacao) {
+        return repository.findAllByAtivoTrue(paginacao).map(DadosListagemMedico::new);
+    }//paginação com lista de 10 medicos por pagina
+
+    @PutMapping
+    @Transactional
+    public void atualizar(@RequestBody @Valid DadosAtualizacaoMedico dados) {
+      var medico = repository.getReferenceById(dados.id());
+      medico.atualizarInformacoes(dados);
+    }//atualização
+
+    // @DeleteMapping("/{id}")
+    // @Transactional
+    // public void excluir(@PathVariable Long id) {
+    //   repository.deleteById(id);
+    // }//deletar
+    @DeleteMapping("/{id}")
+    @Transactional
+    public void excluir(@PathVariable Long id) {
+        var medico = repository.getReferenceById(id);
+        medico.excluir();
+    }//exclusao logica
 }
---
+++ api/src/main/java/med/voll/api/medico/MedicoRepository.java
@@ -0,0 +1,8 @@
+package med.voll.api.medico;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+public interface MedicoRepository extends  JpaRepository<Medico,Long> {
+    Page<Medico> findAllByAtivoTrue(Pageable paginacao);
+
+}
